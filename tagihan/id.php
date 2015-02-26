1ff835301a8e13866c7471f37877d3ded1234f35
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 12:55:35 +0800

    Create id.php

---
+++ tagihan/id.php
@@ -0,0 +1,9 @@
+<?php
+include("/root.php");
+$id = $_REQUEST('id');
+$request = $target . "/pembayaran/pelanggan/id/" . id . "/payable";
+
+$result = file_get_contents($request);
+
+echo $result;
+?>
