7a20883fdf3ce3393578b3980aee911cc9f7c954
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 12:33:32 +0800

    Using 'REQUEST' to retrieve HTTP variable

--- pelanggan/id.php
+++ pelanggan/id.php
@@ -1,7 +1,7 @@
 <?php
-$requestBody = file_get_contents('php://input');
+$id = $_REQUEST['id'];
 
-$request = "http://uvision.whelastic.net/tvkabel/api/pelanggan/id/" . $requestBody; 
+$request = "http://uvision.whelastic.net/tvkabel/api/pelanggan/id/" . $id; 
 $file = file_get_contents($request); 
 
 echo $file; 
