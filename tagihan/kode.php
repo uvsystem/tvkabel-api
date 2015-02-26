cdd68ab4b0140c9fd0914665d92dc79694f3d7db
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 21:42:18 +0800

    Update kode.php

--- tagihan/kode.php
+++ tagihan/kode.php
@@ -5,7 +5,7 @@ $requestBody = file_get_contents("php://input");
 $obj = json_decode($requestBody);
 $request = $target . "/pembayaran/perusahaan/" . $obj->idPerusahaan . "/pelanggan/kode/" . $obj->kode . "/payable";
 
-$response = execute($request, null, ""get);
+$response = execute($request, null, "get");
 
 echo $response;
 ?>
