fd42243b995bb92cf7698bd1fd8c8b98da103feb
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 22:31:32 +0800

    Update root.php

--- root.php
+++ root.php
@@ -1,44 +1,60 @@
 <?php
 $target = "http://uvision.whelastic.net/tvkabel/api";
 
-function execute($url, $data, $method) {
-  $httpHeader = array (
+class Rest {
+  private $_url;
+  private $_data;
+  private $_method;
+  private $httpHeader = array (
     "Accept: application/json",
     "Content-Type: application/json",
   );
 
-  //open curl connection
-  $session = curl_init();
+  function __construct($url, $data, $method) {
+    $this->_url = $url;
+    $this->_data = $data;
+    $this->_method = $method;
+  }
   
-  curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($session, CURLOPT_HTTPHEADER, $httpHeader);
+  public function execute() {
+    //open curl connection
+    $session = curl_init();
+    
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($session, CURLOPT_HTTPHEADER, $this->httpHeader);
+    $this->setUrl($session, $this->url, $this->data, $this->method);  
+    $this->setCurlMethod($session, $this->method);
   
-  curl_setopt($session, CURLOPT_URL, $url);
-  if ((($method == "GET") || ($method == "get")) && (($data != null) && ($data != ""))) {
-    $url .= "?" . $data;
+    //set request body with json if $data is not null
+    if ($this->data != null) {
+      curl_setopt($session, CURLOPT_POSTFIELDS, $this->data);
+    }
+
+    $response = curl_exec($session);
+    
+    curl_close($session);
+
+    return $response;
+  }
+  
+  private function setCurlMethod($session, $method) {
+    if (($method == "DELETE") || ($method == "delete")) {
+      curl_setopt($session, CURLOPT_CUSTOMREQUEST, $method);
+    } else if (($method == "GET") || ($method == "get")) {
+      curl_setopt($session, CURLOPT_HTTPGET, true);
+    } else if (($method == "POST") || ($method == "post")) {
+     curl_setopt($session, CURLOPT_POST, true);
+    } else if (($method == "PUT") || ($method == "put")) {
+      curl_setopt($session, CURLOPT_PUT, true);
+    }
+  }
+
+  private function setUrl($session, $url, $data, $method) {
     curl_setopt($session, CURLOPT_URL, $url);
+    if ((($method == "GET") || ($method == "get")) && (($data != null) && ($data != ""))) {
+      $url .= "?" . $data;
+      curl_setopt($session, CURLOPT_URL, $url);
+    }
   }
-  
-  //set http method
-  if (($method == "DELETE") || ($method == "delete")) {
-    curl_setopt($session, CURLOPT_CUSTOMREQUEST, $method);
-  } else if (($method == "GET") || ($method == "get")) {
-    curl_setopt($session, CURLOPT_HTTPGET, true);
-  } else if (($method == "POST") || ($method == "post")) {
-    curl_setopt($session, CURLOPT_POST, true);
-  } else if (($method == "PUT") || ($method == "put")) {
-    curl_setopt($session, CURLOPT_PUT, true);
-  }
-  //set request body with json if $data is not null
-  if ($data != null) {
-    curl_setopt($session, CURLOPT_POSTFIELDS, $data);
-  }
-
-  $result = curl_exec($session);
-
-  //close connection
-  curl_close($session);
-
-  return $result;
 }
 ?>
