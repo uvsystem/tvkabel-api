ce635a17f9debb58bc65286410bd0f96de3621ad
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 20:27:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include("root.php");
+
+$url = "http://uvs-t001.whelastic.net/api/pelanggan/kode.php";
+
+$data = array(
+  idPerusahaan => "17",
+  kode => "ws01001"
+);
+$json = json_encode($data);
+
+$response = execute($url, $json, "GET");
+
+echo $response;
+?>
