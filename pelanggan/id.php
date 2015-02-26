1e27663890461b0226eaaad837017c3e58d9d513
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 13:14:08 +0800

    Update id.php

--- pelanggan/id.php
+++ pelanggan/id.php
@@ -1,8 +1,8 @@
 <?php
-include("/root.php");
+include("../root.php");
 $id = $_REQUEST['id'];
 
-$request = $target . "/pelanggan/id/" . $id; 
+$request = "http://uvision.whelastic.net/tvkabel/api/pelanggan/id/" . $id; 
 $file = file_get_contents($request); 
 
 echo $file; 
