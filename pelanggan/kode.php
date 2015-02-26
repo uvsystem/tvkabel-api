e82fb6452dc12127a04d1c6aa883dfa1a924c0fc
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 21:48:08 +0800

    Update kode.php

--- pelanggan/kode.php
+++ pelanggan/kode.php
@@ -2,11 +2,10 @@
 include("../root.php");
 
 $requestBody = file_get_contents("php://input");
-
 $obj = json_decode($requestBody);
 $request = $target . "/pelanggan/perusahaan/" . $obj->idPerusahaan . "/kode/" . $obj->kode;
 
-$response = file_get_contents($request);
+$response = execute($request, null, "get");
 
 echo $response;
 ?>
