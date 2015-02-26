89f44d9ebcca6abf84c8367addd55bbcb480911c
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 22:54:00 +0800

    Use rest

--- tagihan/id.php
+++ tagihan/id.php
@@ -2,8 +2,10 @@
 include("../root.php");
 
 $id = $_REQUEST['id'];
-$request = $target . "/pembayaran/pelanggan/id/" . $id . "/payable";
-$response = execute($request, null, "get");
+$url = $target . "/pembayaran/pelanggan/id/" . $id . "/payable";
+
+$rest = new Rest($url, null, "GET");
+$response = $rest->execute();
 
 echo $response;
 ?>
