6072ab8ec3f9acf1c20cd5919a089cb5d1a9b547
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 12:58:08 +0800

    Update id.php

--- tagihan/id.php
+++ tagihan/id.php
@@ -1,9 +1,9 @@
 <?php
 include("/root.php");
 $id = $_REQUEST('id');
-$request = $target . "/pembayaran/pelanggan/id/" . id . "/payable";
 
-$result = file_get_contents($request);
+$request = $target . "/pembayaran/pelanggan/id/" . $id . "/payable";
+$response = file_get_contents($request);
 
-echo $result;
+echo $response;
 ?>
