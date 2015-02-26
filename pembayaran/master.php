8bea59fc693b113e0c9d9c2b3e7bee3e3c300716
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 21:57:19 +0800

    Update master.php

--- pembayaran/master.php
+++ pembayaran/master.php
@@ -1,29 +1,10 @@
 <?php
 include("../root.php");
 
-$requestBody = file_get_contents("php://file");
+$requestBody = file_get_contents("php://input");
 $url = $target . "/pembayaran/master";
 
-$httpHeader = array (
-  "Accept: application/json",
-  "Accept-Encoding: gzip, deflate",
-  "Content-Type: application/json",
-);
+$response = execute($url, $requestBody, "post");
 
-//open curl connection
-$session = curl_init($url);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, 1);
-//set request method as post
-curl_setopt($session, CURLOPT_POST, 1);
-//set request body with json
-curl_setopt($session, CURLOPT_POSTFIELDS, $requestBody);
-//set request header to support json
-curl_setopt($session, CURLOPT_HTTPHEADER, $httpHeader);
-
-$result = curl_exec($session);
-
-//close connection
-curl_close($session);
-
-echo $result;
+echo $response;
 ?>
