2abc50619f00b375724896eadc067a2900f27c15
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 23:25:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 include("root.php");
 
-$url = "http://uvs-t001.whelastic.net/api/tagihan/kode.php";
-
 $data = array(
   idPerusahaan => "17",
   kode => "ws01001"
 );
 $json = json_encode($data);
 
+$url = "http://uvs-t001.whelastic.net/api/tagihan/kode.php";
 $rest = new Rest($url, $json, "GET");
 $response = $rest->execute();
 echo $response;
 
-$rest->set("http://www.google.com", null, "GET");
+$url = "http://uvs-t001.whelastic.net/api/pelanggan/kode.php";
+$rest->set($url, $json, "GET");
 $response = $rest->execute();
 echo $response;
 ?>
