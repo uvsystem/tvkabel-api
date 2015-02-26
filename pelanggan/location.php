6044e440a6c1d477a4e79934333627b0c31245b5
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 21:53:07 +0800

    Update location.php

--- pelanggan/location.php
+++ pelanggan/location.php
@@ -6,24 +6,7 @@ $requestBody = file_get_contents("php://input");
 $obj = json_decode($requestBody);
 $url = $target . "/pelanggan/id" . $obj->id . "/location/" . $obj->latitude . "/" . $obj->longitude;
 
-$httpHeader = array (
-  "Accept: application/json",
-  "Accept-Encoding: gzip, deflate",
-  "Content-Type: application/json",
-);
-
-//open curl connection
-$session = curl_init($url);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, 1);
-//set request method as post
-curl_setopt($session, CURLOPT_POST, 1);
-//set request header to support json
-curl_setopt($session, CURLOPT_HTTPHEADER, $httpHeader);
-
-$response = curl_exec($session);
-
-//close connection
-curl_close($session);
+$response = execute($url, null, "put");
 
 echo $response;
 ?>
