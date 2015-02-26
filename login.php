8b6f3fc348c47314ee6ec4cf2f812c9e3058ef61
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 22:58:30 +0800

    Use rest

--- login.php
+++ login.php
@@ -1,28 +1,11 @@
 <?php
 include("root.php");
-//extract json data from request payload
+
 $requestBody = file_get_contents('php://input');
 $url = $target . "/login";
-$httpHeader = array (
-  "Accept: application/json",
-  "Accept-Encoding: gzip, deflate",
-  "Content-Type: application/json",
-);
 
-//open curl connection
-$session = curl_init($url);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, 1);
-//set request method as post
-curl_setopt($session, CURLOPT_POST, 1);
-//set request body with json
-curl_setopt($session, CURLOPT_POSTFIELDS, $requestBody);
-//set request header to support json
-curl_setopt($session, CURLOPT_HTTPHEADER, $httpHeader);
+$rest = new Rest($url, $requestBody, "POST");
+$response = $rest->execute();
 
-$result = curl_exec($session);
-
-//close connection
-curl_close($session);
-
-echo $result;
+echo $response;
 ?>
