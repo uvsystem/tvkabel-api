8d318b2ab163a70f362402404cda0d0362c702bb
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 19:56:13 +0800

    Update and rename tagihan to tagihan/kode.php

---
+++ tagihan/kode.php
@@ -0,0 +1,11 @@
+<?php
+include("../root.php");
+
+$requestBody = file_get_contents("php://file");
+$obj = json_decode($requestBody);
+$request = $target . "/pembayaran/perusahaan/" . $obj->idPerusahaan . "/pelanggan/kode/" . $obj->kode . "/payable";
+
+$response = file_get_contents($request);
+
+echo $response;
+?>
