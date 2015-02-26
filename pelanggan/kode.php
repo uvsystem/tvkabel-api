6dc470f7cc7bf2ece85294c242b31fa829fc3bcf
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 22:49:03 +0800

    Use rest

--- pelanggan/kode.php
+++ pelanggan/kode.php
@@ -3,9 +3,10 @@ include("../root.php");
 
 $requestBody = file_get_contents("php://input");
 $obj = json_decode($requestBody);
-$request = $target . "/pelanggan/perusahaan/" . $obj->idPerusahaan . "/kode/" . $obj->kode;
+$url = $target . "/pelanggan/perusahaan/" . $obj->idPerusahaan . "/kode/" . $obj->kode;
 
-$response = execute($request, null, "get");
+$rest = new Rest($url, null, "GET");
+$response = $rest->execute();
 
 echo $response;
 ?>
