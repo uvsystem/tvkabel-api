b0f293a25d2e37cbcfe5e717f152d348c7fb984d
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-26 13:10:12 +0800

    Update id.php

--- pelanggan/id.php
+++ pelanggan/id.php
@@ -3,7 +3,7 @@ include("/root.php");
 $id = $_REQUEST['id'];
 
 $request = $target . "/pelanggan/id/" . $id; 
-$result = file_get_contents($request); 
+$file = file_get_contents($request); 
 
-echo $result; 
+echo $file; 
 ?>
