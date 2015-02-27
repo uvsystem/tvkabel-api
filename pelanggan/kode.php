8c003299a8a26a61c4246009d4154bb01593cd24
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-27 12:36:31 +0800

    Debugging

--- pelanggan/kode.php
+++ pelanggan/kode.php
@@ -1,12 +1,15 @@
 <?php
 include("../root.php");
 
-$requestBody = file_get_contents("php://input");
+//$requestBody = file_get_contents("php://input");
+$requestBody = $_GET['kode'];
 
 echo "debug: " . $requestBody . "<br /><br />";
-print_r($_GET);
 
 $obj = json_decode($requestBody);
+
+echo "debug: " . $obj . "<br /><br />";
+
 $url = $target . "/pelanggan/perusahaan/" . $obj->idPerusahaan . "/kode/" . $obj->kode;
 
 $rest = new Rest($url, null, "GET");
