ca993adf6516813376012667b3b676c3fc2efcac
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2015-02-27 12:44:17 +0800

    Update kode.php

--- pelanggan/kode.php
+++ pelanggan/kode.php
@@ -1,15 +1,9 @@
 <?php
 include("../root.php");
 
-//$requestBody = file_get_contents("php://input");
-$requestBody = $_GET['kode'];
-
-echo "debug: " . $requestBody . "<br /><br />";
+$requestBody = file_get_contents("php://input");
 
 $obj = json_decode($requestBody);
-
-echo "debug: " . $obj . "<br /><br />";
-
 $url = $target . "/pelanggan/perusahaan/" . $obj->idPerusahaan . "/kode/" . $obj->kode;
 
 $rest = new Rest($url, null, "GET");
